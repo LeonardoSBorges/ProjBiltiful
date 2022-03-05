c3fbef95f2255cc548e1a5e4543bb276a278fc07
Leonardo Borges <ldssborges@hotmail.com>
2022-03-05 15:16:27 -0300

    Adicionado validacoes, MenuCadastros alteracoes em client e program

--- CadastrosBasicos/Validacoes.cs
+++ CadastrosBasicos/Validacoes.cs
@@ -8,7 +8,54 @@ namespace CadastrosBasicos
 {
     public class Validacoes
     {
-        public static bool IsValid(string cpf)
+        public static bool CalcudaData(DateTime data)
+        {
+            DateTime dateNow = DateTime.Now;
+            int calculaData;
+
+            calculaData = dateNow.Year - data.Year  ;
+
+            if (calculaData == 18 && data.Month > dateNow.Month || calculaData == 18 && data.Month == dateNow.Month && data.Day > dateNow.Day)
+                calculaData--;
+
+            return calculaData >= 18 ? true : false;
+        }
+
+        public static bool ValidarCnpj(string cnpj)
+        {
+            int[] multiplicador1 = new int[12] { 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplicador2 = new int[13] { 6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int soma;
+            int resto;
+            string digito;
+            string tempCnpj;
+            cnpj = cnpj.Trim();
+            cnpj = cnpj.Replace(".", "").Replace("-", "").Replace("/", "");
+            if (cnpj.Length != 14)
+                return false;
+            tempCnpj = cnpj.Substring(0, 12);
+            soma = 0;
+            for (int i = 0; i < 12; i++)
+                soma += int.Parse(tempCnpj[i].ToString()) * multiplicador1[i];
+            resto = (soma % 11);
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+            digito = resto.ToString();
+            tempCnpj = tempCnpj + digito;
+            soma = 0;
+            for (int i = 0; i < 13; i++)
+                soma += int.Parse(tempCnpj[i].ToString()) * multiplicador2[i];
+            resto = (soma % 11);
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+            digito = digito + resto.ToString();
+            return cnpj.EndsWith(digito);
+        }
+        public static bool ValidarCpf(string cpf)
         {
             int[] multiplicador1 = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
             int[] multiplicador2 = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
