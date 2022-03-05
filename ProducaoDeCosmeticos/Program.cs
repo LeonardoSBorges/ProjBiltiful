b10ec1f177d8b5969452aab1772a629729814005
Leonardo Borges <ldssborges@hotmail.com>
2022-03-04 21:48:04 -0300

    Iniciando projeto biltiful

---
+++ ProducaoDeCosmeticos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProducaoDeCosmeticos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
