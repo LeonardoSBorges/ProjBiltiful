41170d7fe9b09ee7fdf8c66f467d67da12770b2d
Leonardo Borges <ldssborges@hotmail.com>
2022-03-05 09:37:29 -0300

    Adicionando menu basico

--- ProjBiltiful/Program.cs
+++ ProjBiltiful/Program.cs
@@ -7,7 +7,37 @@ namespace ProjBiltiful
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!!");
+            int value = -1;
+            while (value!=0)
+            {
+                Console.Write(@"1. Cadastros
+2. Vendas
+2. Compra de Materia-Prima
+3. Producao
+0 - Sair
+Insira uma opcao valida: 
+");
+                value = int.Parse(Console.ReadLine());
+
+                 
+                    
+                switch (value)
+                {
+                    case 0:
+                        // sair
+                        break;
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+
+                }
+
+                Console.ReadKey();
+                Console.Clear();
+            }
         }
     }
 }
