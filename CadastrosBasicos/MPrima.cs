66033487f3fbab1002c0a6f43e6cf1021cd5ffa3
Leonardo Borges <ldssborges@hotmail.com>
2022-03-05 10:05:14 -0300

    Inserindo nova Branch

--- CadastrosBasicos/MPrima.cs
+++ CadastrosBasicos/MPrima.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CadastrosBasicos
 {
-    internal class MPrima
+    public class MPrima
     {
         public string id { get; set; }
         public string nome { get; set; }
