513436c0ee14455f56c2a4086326b21a3a3ac0f7
Leonardo Borges <ldssborges@hotmail.com>
2022-03-05 17:23:21 -0300

    Ajuste de projetos

--- Arquivos/Arquivos.cs
+++ Arquivos/Arquivos.cs
@@ -4,9 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using CadastrosBasicos;
 
-namespace ProjBiltiful
+namespace ManipulaArquivos
 {
     public class Arquivos
     {
