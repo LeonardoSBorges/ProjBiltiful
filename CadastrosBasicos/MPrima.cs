0d29d4a3d2f9b0c0c7637b641a8fc5eafb936184
andap1988 <and.dinho_2008@hotmail.com>
2022-03-05 09:49:29 -0300

    MPrima

---
+++ CadastrosBasicos/MPrima.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastrosBasicos
+{
+    internal class MPrima
+    {
+        public string id { get; set; }
+        public string nome { get; set; }
+        public string ucompra { get; set; }
+        public string dcadastro { get; set; }
+        public char situacao { get; set; }
+
+        // nome arquivo: Materia.dat
+
+    }
+}
