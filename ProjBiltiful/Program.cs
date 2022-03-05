d4fdc3a5a675e5d79398e7ddd2b06158f727027e
Junior Pires <jpirees@hotmail.com>
2022-03-05 16:37:38 -0300

    Adicionado metódo cadastrar de Venda e ItemVenda

--- ProjBiltiful/Program.cs
+++ ProjBiltiful/Program.cs
@@ -1,44 +1,91 @@
 ﻿using System;
 using VendasProdutos;
 using CadastrosBasicos;
+using System.Globalization;
+using System.Collections.Generic;
+
 namespace ProjBiltiful
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int value = -1;
-            while (value!=0)
+            string opcao;
+
+            do
             {
-                Console.Write(@"1. Cadastros
-2. Vendas
-2. Compra de Materia-Prima
-3. Producao
-0 - Sair
-Insira uma opcao valida: 
-");
-                value = int.Parse(Console.ReadLine());
-
-                 
-                    
-                switch (value)
+                switch (opcao = Menu())
                 {
-                    case 0:
-                        // sair
-                        break;
-                    case 1:
-
-                        break;
-                    case 2:
-                        break;
-                    case 3:
+                    case "1":
+                        NovaVenda();
                         break;
 
+                    case "2":
+                        ConsultarVenda();
+                        break;
+
+                    case "3":
+                        Environment.ExitCode = 0;
+                        break;
+
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Opção inválida");
+                        break;
                 }
 
-                Console.ReadKey();
+            } while (opcao != "3");
+        }
+
+        static string Menu()
+        {
+            Console.WriteLine("========== Vendas ==========");
+            Console.WriteLine("[1] Nova venda");
+            Console.WriteLine("[2] Consultar Venda");
+            Console.WriteLine("[3] Voltar");
+
+            return Console.ReadLine();
+        }
+
+        static void NovaVenda()
+        {
+            int id = 1;
+
+            Venda venda = new Venda();
+            ItemVenda itemVenda = new ItemVenda();
+
+            List<ItemVenda> itens = new List<ItemVenda>();
+
+            for (int i = 0; i < 3; i++)
+            {
                 Console.Clear();
+
+                Console.WriteLine("Informe o produto: ");
+                string produto = Console.ReadLine();
+
+                Console.WriteLine("Informe a quantidade do produto: ");
+                int quantidade = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Informe o valor do produto: ");
+                decimal vUnitario = Decimal.Parse(Console.ReadLine());
+
+                itens.Add(new ItemVenda(id, produto, quantidade, vUnitario));
             }
+
+            itens.ForEach(item => venda.VTotal += item.TItem);  
+            
+            venda.Id = id;
+            venda.Cliente = "123.456.789-00";
+            venda.DVenda = DateTime.Now;
+
+            itemVenda.Cadastrar(itens);
+
+            venda.Cadastrar();
+        }
+
+        static void ConsultarVenda()
+        {
+
         }
     }
 }
