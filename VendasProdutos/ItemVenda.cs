5dfc67514ff5359991524957b223abdc043d3530
Junior Pires <jpirees@hotmail.com>
2022-03-05 12:00:53 -0300

    Classes Venda e ItemVenda adicionada

---
+++ VendasProdutos/ItemVenda.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace VendasProdutos
+{
+    public class ItemVenda
+    {
+        public int Id { get; set; }
+        public string Produto { get; set; }
+        public int Qtd { get; set; }
+        public decimal VUnitario { get; set; }
+        public decimal TItem { get; set; }
+
+        public void Cadastrar()
+        {
+        }
+
+        public void Localizar()
+        {
+        }
+
+        public void Excluir()
+        {
+        }
+
+        public void ImpressaoPorRegistro()
+        {
+        }
+    }
+}
