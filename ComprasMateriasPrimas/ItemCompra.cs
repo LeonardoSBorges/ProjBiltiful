8845e297eadefd1ae6faf32849f7bdfb9a66c008
laurammorais <laulaumorais@gmail.com>
2022-03-06 22:12:18 -0300

    Salvando Arquivo do ItemCompra

---
+++ ComprasMateriasPrimas/ItemCompra.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ComprasMateriasPrimas
+{
+    public class ItemCompra
+    {
+        public ItemCompra(int id, DateTime dCompra, string mPrima, float qtd, float vUnitario)
+        {
+            Id = id;
+            DCompra = dCompra;
+            MPrima = mPrima;
+            Qtd = qtd;
+            VUnitario = vUnitario;
+            TItem = qtd * vUnitario;
+        }
+
+        public int Id { get; set; } //5 campos
+        public DateTime DCompra { get; set; } //8 campos
+        public string MPrima { get; set; } //6 campos
+        public float Qtd { get; set; } //5 campos
+        public float VUnitario { get; set; } //5 campos
+        public float TItem { get; set; } //6 campos
+
+        public bool TItemValido() => TItem <= 9999.99;
+    }
+}
---
+++ ComprasMateriasPrimas/ManipulaArquivosCompraMP.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace ComprasMateriasPrimas
+{
+    public class ManipulaArquivosCompraMP
+    {
+        public static string Caminho { get; set; }
+        public static string ArquivoCompra { get; set; }
+        public static string ArquivoItemCompra { get; set; }
+        public ManipulaArquivosCompraMP()
+        {
+            Caminho = GetCaminhoDiretorio();
+            ArquivoCompra = GetCaminhoArquivo("Compra.dat");
+            ArquivoItemCompra = GetCaminhoArquivo("ItemCompra.dat");
+        }
+        public static void Salvar(List<ItemCompra> itensCompra)
+        {
+            StreamWriter sw = File.AppendText(ArquivoItemCompra);
+            foreach (ItemCompra item in itensCompra)
+            {
+                string linha = $"{item.Id,-5}" +
+                    $"{item.DCompra:ddMMyyyy}" +
+                    $"{item.MPrima,-6}" +
+                    $"{item.Qtd.ToString().Replace(".", "").Replace(",", ""), -5}" +
+                    $"{item.VUnitario.ToString().Replace(".", "").Replace(",", ""), -5}" +
+                    $"{item.TItem.ToString().Replace(".", "").Replace(",", ""), -6}";
+
+                sw.WriteLine(linha);
+            }
+            sw.Close();
+        }
+            
+        static string GetCaminhoDiretorio()
+        {
+            string caminho = Path.Combine(Directory.GetCurrentDirectory(), "ProjBiltiful", "Compra");
+            if (!Directory.Exists(caminho)) Directory.CreateDirectory(caminho);
+            return caminho;
+        }
+
+        string GetCaminhoArquivo(string arquivo)
+        {
+            string arquivoDat = Path.Combine(Caminho, arquivo);
+            if (!File.Exists(arquivoDat))
+                File.Create(arquivoDat).Close();
+            return arquivoDat;
+        }
+
+    }
+}
