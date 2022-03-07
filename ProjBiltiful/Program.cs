35d30d515325d9e2bd1e0b1285c151db761c2dcc
viniciusyashuda <viniciusyashuda008@gmail.com>
2022-03-07 12:16:03 -0300

    producao

--- ProjBiltiful/Program.cs
+++ ProjBiltiful/Program.cs
@@ -10,41 +10,52 @@ namespace ProjBiltiful
     {
         static void Main(string[] args)
         {
-            //Este menu sera utilizado para testes
-            int value = -1;
-            while (value != 0)
-            {
-                Console.Write(@"============= BITIFUL =============
-1. Cadastros
-2. Vendas
-2. Compra de Materia-Prima
-3. Producao
-0 - Sair
-Insira uma opcao valida: 
-");
-                value = int.Parse(Console.ReadLine());
+            var cultureInformation = new CultureInfo("pt-BR");
+            cultureInformation.NumberFormat.CurrencySymbol = "R$";
+            CultureInfo.DefaultThreadCurrentCulture = cultureInformation;
+            CultureInfo.DefaultThreadCurrentUICulture = cultureInformation;
+            Producao producao = new Producao();
+            producao.Menu();
 
-
-
-                switch (value)
-                {
-                    case 0:
-                        // sair
-                        break;
-                    case 1:
-                        //Cadastrar
-                        MenuCadastros.SubMenu();
-                        break;
-                    case 2:
-                        break;
-                    case 3:
-                        break;
-
-                }
-
-                Console.ReadKey();
-                Console.Clear();
-            }
         }
     }
 }
+
+//            //Este menu sera utilizado para testes
+//            int value = -1;
+//            while (value != 0)
+//            {
+//                Console.Write(@"============= BITIFUL =============
+//1. Cadastros
+//2. Vendas
+//2. Compra de Materia-Prima
+//3. Producao
+//0 - Sair
+//Insira uma opcao valida: 
+//");
+//                value = int.Parse(Console.ReadLine());
+
+
+
+//                switch (value)
+//                {
+//                    case 0:
+//                        // sair
+//                        break;
+//                    case 1:
+//                        //Cadastrar
+//                        MenuCadastros.SubMenu();
+//                        break;
+//                    case 2:
+//                        break;
+//                    case 3:
+//                        break;
+
+//                }
+
+//                Console.ReadKey();
+//                Console.Clear();
+//            }
+//        }
+//    }
+//}
