db557ea54313234630326e863a58f0cbcaa64477
Junior Pires <jpirees@hotmail.com>
2022-03-07 11:09:51 -0300

    atualização e correções

--- VendasProdutos/ItemVenda.cs
+++ VendasProdutos/ItemVenda.cs
@@ -25,6 +25,14 @@ namespace VendasProdutos
             TotalItem = quantidade * valorUnitario;
         }
 
+        public ItemVenda(string produto, int quantidade, decimal valorUnitario)
+        {
+            Produto = produto;
+            Quantidade = quantidade;
+            ValorUnitario = valorUnitario;
+            TotalItem = quantidade * valorUnitario;
+        }
+
         public override string ToString()
         {
             return $"{Id.ToString().PadLeft(5, '0')}\t{Produto}\t{Quantidade.ToString().PadLeft(3, '0')}\t{ValorUnitario.ToString("000.00").TrimStart('0')}\t\t{TotalItem.ToString("0000.00").TrimStart('0')}";
@@ -50,16 +58,42 @@ namespace VendasProdutos
             }
         }
 
-        public void Localizar()
+        public List<ItemVenda> Localizar(int idVenda)
         {
+
+            try
+            {
+                StreamReader sr = new StreamReader(caminho.ArquivoItemVenda);
+
+                List<ItemVenda> itens = new List<ItemVenda>();
+
+                string line;
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    if (int.Parse(line.Substring(0, 5).TrimStart('0')) == idVenda)
+                    {
+                        string produto = line.Substring(5, 13);
+                        string quantidade = line.Substring(18, 3);
+                        string valorUnitario = line.Substring(21, 5);
+
+                        Decimal.TryParse(valorUnitario.Insert(valorUnitario.Length - 2, ","), out decimal vUnitario);
+
+                        itens.Add(new ItemVenda(produto, int.Parse(quantidade), vUnitario));
+                    }
+                }
+
+                sr.Close();
+
+                return itens;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+
+            return null;
         }
 
-        public void Excluir()
-        {
-        }
-
-        public void ImpressaoPorRegistro()
-        {
-        }
     }
 }
--- VendasProdutos/MenuVendas.cs
+++ VendasProdutos/MenuVendas.cs
@@ -22,8 +22,6 @@ namespace VendasProdutos
                 Console.WriteLine("=============== VENDAS ===============");
                 Console.WriteLine("1. Nova venda");
                 Console.WriteLine("2. Consultar Venda");
-                Console.WriteLine("3. Consultar Produtos de uma venda");
-                Console.WriteLine("4. Excluir Venda");
                 Console.WriteLine("0. Voltar");
 
                 switch (opcao = Console.ReadLine())
@@ -34,14 +32,6 @@ namespace VendasProdutos
 
                     case "2":
                         LocalizarVenda();
-                        break;
-
-                    case "3":
-                        LocalizarItens();
-                        break;
-
-                    case "4":
-                        ExcluirVenda();
                         break;
 
                     case "0":
@@ -86,17 +76,11 @@ namespace VendasProdutos
                 Console.WriteLine("\nDigite o Código do Produto:");
                 string produto = Console.ReadLine();
 
-                //aqui vai ter que alterar pra fazer a verificação se o produto existe no arquivo de produtos e depois mostrar na tela qual o produto escolhido
-
-
                 Console.WriteLine("\nInforme a quantidade:");
                 int qtd = int.Parse(Console.ReadLine());
 
                 Console.WriteLine("\nInforme o valor do produto: ");
                 decimal valor = decimal.Parse(Console.ReadLine());
-
-                //e aqui vai pegar no arquivo de produtos o valor referente ao produto que buscamos. Não vai precisar digitar o valor. 
-
 
                 Console.Clear();
 
@@ -178,133 +162,34 @@ namespace VendasProdutos
             Console.Clear();
 
             Venda venda = new Venda();
+            ItemVenda itemVenda = new ItemVenda();
 
             Console.WriteLine("Informe a venda que deseja buscar: ");
             int.TryParse(Console.ReadLine(), out int id);
             Console.WriteLine();
+
             venda = venda.Localizar(id);
 
             if (venda != null)
             {
-                Console.WriteLine(venda.ToString());
-                Console.WriteLine("\nPressione ENTER para voltar ao menu...");
-                Console.ReadLine();
+                List<ItemVenda> itens = itemVenda.Localizar(venda.Id);
+
+                Console.Write($"\nVenda Nº {venda.Id.ToString().PadLeft(5, '0')}\tData: {venda.DataVenda.ToString("dd/MM/yyyy")}");
+                Console.WriteLine("\n\n");
+
+                Console.WriteLine("Id\tProduto\t\tQtd\tV.Unitário\tT.Item");
+                Console.WriteLine("------------------------------------------------------");
+                itens.ForEach(item => Console.WriteLine(item.ToString()));
+                Console.WriteLine("------------------------------------------------------");
+                Console.WriteLine($"\t\t\t\t\t\t{venda.ValorTotal.ToString("#.00")}");
+
+                Console.WriteLine("\n\n");
             }
             else
             {
                 Console.WriteLine("venda não registrada!\nPressione ENTER para voltar ao menu...");
                 Console.ReadLine();
             }
-        }
-
-        private static void LocalizarItens()
-        {
-            string line;
-            try
-            {
-                StreamReader sr = new StreamReader("C:\\Users\\Everton Fabricio\\source\\repos\\ProjBiltiful\\ProjBiltiful\\bin\\Debug\\net5.0\\ProjBiltiful\\Venda\\ItemVenda.dat");
-                //coloquei o caminho inteiro pq não sei como fazer pra chamar caminho do jeito que o Junior fez.
-
-                Console.Write("Pedido Numero: ");
-                string busca = Console.ReadLine();
-                line = sr.ReadLine();
-
-                while (line != null)
-                {
-                    if (line.Substring(0, 5) == busca)
-                    {
-                        Console.WriteLine("Quant\t   Produto\tVal.Unit.  Val.Total");
-                        Console.Write(line.Substring(18, 3).TrimStart('0'));
-                        Console.Write("\t" + line.Substring(5, 13));
-                        Console.Write("\t  " + line.Substring(21, 5).TrimStart('0'));
-                        Console.WriteLine("\t     " + line.Substring(26, 5).TrimStart('0'));
-                        Console.WriteLine();
-                    }
-                    line = sr.ReadLine();
-                }
-                sr.Close();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Exception: " + e.Message);
-            }
-            finally
-            {
-                Console.WriteLine("Busca Finalizada.\nPressione ENTER para voltar");
-            }
-            Console.ReadKey();
-        }
-
-        private static void ExcluirVenda()
-        {
-            Console.WriteLine("Informe a Venda que deseja excluir.");
-            Console.Write("Venda Numero: ");
-            string busca = Console.ReadLine();
-            try //para arquivo Venda.dat
-            {
-                string ArqCerto = "C:\\Users\\Everton Fabricio\\source\\repos\\ProjBiltiful\\ProjBiltiful\\bin\\Debug\\net5.0\\ProjBiltiful\\Venda\\Venda.dat";
-                string ArqTemp = "C:\\Users\\Everton Fabricio\\source\\repos\\ProjBiltiful\\ProjBiltiful\\bin\\Debug\\net5.0\\ProjBiltiful\\Venda\\Temp.dat";
-                //Mesma coisa que no localizar item. Eu não sei chamar o caminho do jeito que o Junior fez! Depois que chamar daquele jeito, apaga isso aqui.
-
-                StreamReader sr = new StreamReader(ArqCerto);
-                StreamWriter sw = new StreamWriter(ArqTemp);
-
-                string line = sr.ReadLine();
-
-                while (line != null)
-                {
-                    if (line.Substring(0, 5) != busca)
-                    {
-                        sw.WriteLine(line);
-                    }
-                    line = sr.ReadLine();
-                }
-
-                sr.Close();
-                sw.Close();
-                File.Delete(ArqCerto);
-                File.Copy(ArqTemp, ArqCerto);
-                File.Delete(ArqTemp);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Exception: " + e.Message);
-            }
-
-            try //para arquivo ItemVenda.dat
-            {
-                string ArqCerto = "C:\\Users\\Everton Fabricio\\source\\repos\\ProjBiltiful\\ProjBiltiful\\bin\\Debug\\net5.0\\ProjBiltiful\\Venda\\ItemVenda.dat";
-                string ArqTemp = "C:\\Users\\Everton Fabricio\\source\\repos\\ProjBiltiful\\ProjBiltiful\\bin\\Debug\\net5.0\\ProjBiltiful\\Venda\\Temp.dat";
-                //Mesma coisa que no localizar item. Eu não sei chamar o caminho do jeito que o Junior fez! Depois que chamar daquele jeito, apaga isso aqui.
-
-                StreamReader sr = new StreamReader(ArqCerto);
-                StreamWriter sw = new StreamWriter(ArqTemp);
-                string line = sr.ReadLine();
-                while (line != null)
-                {
-                    if (line.Substring(0, 5) != busca)
-                    {
-                        sw.WriteLine(line);
-                    }
-                    line = sr.ReadLine();
-                }
-
-                sr.Close();
-                sw.Close();
-                File.Delete(ArqCerto);
-                File.Copy(ArqTemp, ArqCerto);
-                File.Delete(ArqTemp);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Exception: " + e.Message);
-            }
-
-            finally
-            {
-                Console.WriteLine("Operação realizada com sucesso.\nPressione ENTER para voltar");
-            }
-            Console.ReadKey();
         }
 
     }
