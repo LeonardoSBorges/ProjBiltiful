6520a8748a91bc40bffba6de59e47b1106f40af1
Matheus Miranda <matheus.miiranda@hotmail.com>
2022-03-07 19:30:28 -0300

    Adicao do Cadastro de Compra

---
+++ ComprasMateriasPrimas/ItemCompra.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComprasMateriasPrimas
+{
+    public class ItemCompra
+    {
+        public int Id { get; set; }
+        public DateTime DCompra { get; set; } //8 campos
+        public string MPrima { get; set; } //6 campos
+        public float Qtd { get; set; } //5 campos
+        public float VUnitario { get; set; } //5 campos
+        public float TItem { get; set; } //6 campos
+    }
+}
